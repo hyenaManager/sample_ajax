8761a8a980fbf09ed7327444ea1ea0a110072421
hyena <mesutkee@gmail.com>
2023-04-21 15:44:45 +0630

    create user creation form and model

--- ajaxTest/models.py
+++ ajaxTest/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    twitter_handle = models.CharField(max_length=50, blank=True)
+    facebook_handle = models.CharField(max_length=50, blank=True)
+    instagram_handle = models.CharField(max_length=50, blank=True)
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
--- ajaxTest/views.py
+++ ajaxTest/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
 
 # Create your views here.
 
 def get(request):
     return render(request,'base.html',{'view':"here is ajax"})
 
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
