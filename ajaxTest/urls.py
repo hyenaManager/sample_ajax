a6282671b346876195f77b8216defcf1a867c8f6
hyena <mesutkee@gmail.com>
2023-04-22 21:26:27 +0630

    added pillow

--- ajaxTest/urls.py
+++ ajaxTest/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,include
 from .views import get,register
 urlpatterns = [
     path('ajax',get,name="get"),
-    path('register',register)
+    path('register',register,name="register")
 ]
